da8fe2c6dd2eb0a887d4646d6b849a9e0edcbec4
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-17 11:27:31 +0800

    Add project styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*/{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      red: 'hsl(0, 100%, 74%)',
+      green: 'hsl(154, 59%, 51%)',
+      blue: 'hsl(248, 32%, 49%)',
+      grey: 'hsl(249, 10%, 26%)',
+      white: 'hsl(246, 25%, 77%)',
+    },
   },
   plugins: [],
 };
+
+/*
+- Mobile: 375px
+- Desktop: 1440px
+*/
