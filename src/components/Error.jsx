db81f120a22e347deea37333a7ca68386e4e0f5a
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-17 17:32:08 +0800

    Error styles

---
+++ src/components/Error.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+function Error({ message }) {
+  return <p className='text-red text-xs text-right pt-2'>{message}</p>;
+}
+
+export default Error;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
+
 import Button from './Button';
+import Error from './Error';
+
+import iconError from '/icon-error.svg';
 
 const VALID_NAME = str => /^[a-z ,.'-]+$/i.test(str);
 const VALID_EMAIL = str => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str);
@@ -12,7 +16,7 @@ function Form() {
     password: '',
   });
   const [errors, setErrors] = useState({});
-  const [submit, setSubmit] = useState(false);
+  const [submit, setSubmit] = useState(false); // use submit for success message
 
   function handleValidate(input) {
     const errors = {};
@@ -65,41 +69,112 @@ function Form() {
 
       <form
         onSubmit={handleSubmit}
-        className='bg-white rounded-lg p-6 sm:p-10 grid grid-cols-1 gap-4 sm:gap-6 relative after:absolute after:h-full after:w-full after:bg-grey/30 after:top-1.5 after:left-0 after:-z-50 after:rounded-lg '
+        className='bg-white rounded-lg p-6 sm:p-10 grid grid-cols-1 gap-4 sm:gap-6 relative after:absolute after:h-full after:w-full after:bg-grey/30 after:top-1.5 after:left-0 after:-z-50 after:rounded-lg'
       >
-        <input
-          placeholder='First Name'
-          autoFocus={true}
-          type='text'
-          name='first'
-          value={input.first}
-          onChange={handleInput}
-          className='border border-grey/20 py-3 px-4 rounded-md  focus:outline focus:outline-violet/80 transition-colors placeholder:text-grey'
-        />
-        <input
-          placeholder='Last Name'
-          type='text'
-          name='last'
-          value={input.last}
-          onChange={handleInput}
-          className='border border-grey/20 py-3 px-4 rounded-md  focus:outline focus:outline-violet/80 transition-colors placeholder:text-grey'
-        />
-        <input
-          placeholder='Email Address'
-          type='text'
-          name='email'
-          value={input.email}
-          onChange={handleInput}
-          className='border border-grey/20 py-3 px-4 rounded-md  focus:outline focus:outline-violet/80 transition-colors placeholder:text-grey'
-        />
-        <input
-          placeholder='Password'
-          type='password'
-          name='password'
-          value={input.password}
-          onChange={handleInput}
-          className='border border-grey/20 py-3 px-4 rounded-md  focus:outline focus:outline-violet/80 transition-colors placeholder:text-grey'
-        />
+        <div>
+          {/* first name */}
+          <div className='relative'>
+            <input
+              placeholder='First Name'
+              autoFocus={true}
+              type='text'
+              name='first'
+              value={input.first}
+              onChange={handleInput}
+              className={`border-2 border-grey/20 py-3 px-4 rounded-md  focus:outline-2 transition-colors placeholder:text-grey w-full ${
+                !errors.first
+                  ? 'focus:outline-violet/80 '
+                  : 'focus:outline-red/80 border-red/80 border-2'
+              }`}
+            />
+            {errors.first && (
+              <img
+                src={iconError}
+                alt='error icon'
+                className='absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2'
+              />
+            )}
+          </div>
+          {errors.first && <Error message={errors.first} />}
+        </div>
+
+        {/* last name */}
+        <div>
+          <div className='relative'>
+            <input
+              placeholder='Last Name'
+              type='text'
+              name='last'
+              value={input.last}
+              onChange={handleInput}
+              className={`border-2 border-grey/20 py-3 px-4 rounded-md  focus:outline-2 transition-colors placeholder:text-grey w-full ${
+                !errors.last
+                  ? 'focus:outline-violet/80 '
+                  : 'focus:outline-red/80 border-red/80 border-2'
+              }`}
+            />
+            {errors.last && (
+              <img
+                src={iconError}
+                alt='error icon'
+                className='absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2'
+              />
+            )}
+          </div>
+          {errors.last && <Error message={errors.last} />}
+        </div>
+
+        {/* email address */}
+        <div>
+          <div className='relative'>
+            <input
+              placeholder='Email Address'
+              type='text'
+              name='email'
+              value={input.email}
+              onChange={handleInput}
+              className={`border-2 border-grey/20 py-3 px-4 rounded-md  focus:outline-2 transition-colors placeholder:text-grey w-full ${
+                !errors.email
+                  ? 'focus:outline-violet/80 '
+                  : 'focus:outline-red/80 border-red/80 border-2'
+              }`}
+            />
+            {errors.email && (
+              <img
+                src={iconError}
+                alt='error icon'
+                className='absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2'
+              />
+            )}
+          </div>
+          {errors.email && <Error message={errors.email} />}
+        </div>
+
+        {/* password */}
+        <div>
+          <div className='relative'>
+            <input
+              placeholder='Password'
+              type='password'
+              name='password'
+              value={input.password}
+              onChange={handleInput}
+              className={`border-2 border-grey/20 py-3 px-4 rounded-md  focus:outline-2 transition-colors placeholder:text-grey w-full ${
+                !errors.password
+                  ? 'focus:outline-violet/80 '
+                  : 'focus:outline-red/80 border-red/80 border-2'
+              }`}
+            />
+            {errors.password && (
+              <img
+                src={iconError}
+                alt='error icon'
+                className='absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2'
+              />
+            )}
+          </div>
+          {errors.password && <Error message={errors.password} />}
+        </div>
         <Button className='bg-green text-white uppercase focus:ring focus:ring-green/50 outline-none'>
           Claim your free trial
         </Button>
